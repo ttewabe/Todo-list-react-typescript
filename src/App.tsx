eb8e28c3af16c71b7bd59a2a7b9f0d913482ae54
Tewabe Tadege <ethiohabesha412@gmail.com>
2023-07-23 22:06:20 -0700

    todo-list using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState } from 'react';
+
+type TodoItem = {
+  title: string;
+  description: string;
+  isDone: boolean;
+  id: string;
+};
+
+const App: React.FC = () => {
+  const [todoList, setTodoList] = useState<TodoItem[]>([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [id, setId] = useState('');
+
+  const handleAddItem = () => {
+    if (title && description && id) {
+      const newItem: TodoItem = {
+        isDone: false,
+        title,
+        description,
+        id,
+      };
+
+      setTodoList([...todoList, newItem]);
+
+      // To clear input fields
+      setTitle('');
+      setDescription('');
+      setId('');
+    }
+  };
+
+  const handleCheckboxChange = (index: number, checked: boolean) => {
+    const updatedTodoList = [...todoList];
+    updatedTodoList[index].isDone = checked;
+    setTodoList(updatedTodoList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+        />
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Description"
+        />
+        <input
+          type="text"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          placeholder="ID"
+        />
+        <button onClick={handleAddItem}>Add Item</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Is Done</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todoList.map((item, index) => (
+            <tr key={index} className={item.isDone ? 'highlighted' : ''}>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={item.isDone}
+                  onChange={(e) => handleCheckboxChange(index, e.target.checked)}
+                />
+              </td>
+              <td>{item.title}</td>
+              <td>{item.description}</td>
+              <td>{item.id}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
+
